c7c4aa61be9cb7a0ab725cc7f8ae4c70a51bf6fd
Yashu Mittal <mittalyashu77@gmail.com>
2019-02-09 02:13:33 +0530

    inital commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const Discord = require('discord.js')
+require('dotenv').config()
+const app = express()
+const client = new Discord.Client()
+const channelInput = process.env.channelInput
+const channelOutput = process.env.channelOutput
+
+app.listen(process.env.PORT)
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('guildMemberAdd', member => {
+	let userId = member.user.id
+	let userBot = member.user.bot
+	if (!userBot) {
+		member.guild.channels.get(channelOutput).send(
+			'<@!' +
+				userId +
+				'> ' +
+				getWelcomeMessage().then(res => {
+          console.log(res);
+					return res;
+				}).catch(err => {
+          console.log(err);
+        })
+		)
+	}
+})
+
+async function fetchAllMessages() {
+	let greetMessagesList = []
+	const channel = client.channels.find(channel => channel.id === channelInput)
+	const channelMessage = await channel.fetchMessages()
+	try {
+		channelMessage.map(message => {
+			greetMessagesList.push(message.content)
+		})
+		return (greetMessagesList = greetMessagesList.reverse())
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+async function getWelcomeMessage() {
+	let greetMessagesList = []
+	fetchAllMessages()
+		.then(result => {
+			return (greetMessagesList = result)
+		})
+		.catch(error => {
+			console.log(error)
+		})
+	console.log(greetMessagesList)
+	console.log(greetMessagesList.length)
+	const number = randomNumber(1, greetMessagesList.length)
+	console.log(number + ': ' + greetMessagesList[number])
+	return greetMessagesList[number]
+}
+
+function randomNumber(min, max) {
+	// TODO Make this function more functionable
+	return Math.floor(Math.random() * (max - min)) + min
+}
+
+client.login(process.env.TOKEN)
---
+++ welcomeMessage.js
@@ -0,0 +1,11 @@
+module.exports = [
+  "1",
+	"Welcome",
+	"Glad you joined!",
+	"Thank you for joining the discord server.",
+	"You were always part of this server",
+	"Nice to meet you",
+	"I knew you will join my server",
+	"Glad, we finally meet.",
+	"What a plesant day."
+]
