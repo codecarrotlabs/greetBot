931d0d467de037097b3279ad2ae9fc3ad399f4d0
Yashu Mittal <mittalyashu77@gmail.com>
2019-02-12 02:21:32 +0530

    Add a note for global greetMessageList variable

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const client = new Discord.Client()
 app.get('/', (req, res) => {
 	res.send('Your Bot is running at port ' + process.env.PORT || 3000)
 })
+
+// NOTE Greet message Array accessible globally
 let greetMessagesList = []
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`)
